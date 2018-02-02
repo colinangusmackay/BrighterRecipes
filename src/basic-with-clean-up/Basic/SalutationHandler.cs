81c8df861b0a00d59652efc0b4771a033adb4933
Colin Mackay <github@colinmackay.scot>
2018-02-02 14:33:16 +0000

    Code for Ensuring Dependencies are Disposed.

---
+++ src/basic-with-clean-up/Basic/SalutationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Paramore.Brighter;
+
+namespace Basic
+{
+    public class SalutationHandler : RequestHandler<SalutationCommand>, IDisposable
+    {
+        public override SalutationCommand Handle(SalutationCommand command)
+        {
+            Console.WriteLine($"Greetings, {command.Name}.");
+            return base.Handle(command);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("I'm being disposed.");
+        }
+    }
+}
